3cdf7f5ecc7a2242c6923dc295f0ecdaa57fe2df
wyaich@outlook.fr <wyaich@outlook.fr>
2016-12-19 23:19:52 +0100

    Ajouter le plugin exec pour lancer l'appli directement par exec:java -Dexec.mainClass="com.javahelps.jpa.Main"

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JPA-2.1
 
 http://www.javahelps.com/2015/08/jpa-hello-world-using-hibernate.html
+
+2 methodes pour lancer le programme :
+* clean package pour générer le jar excécutable (config faite dans le pom.xml) puis le lancer avec java -jar
+* directement exec:java -Dexec.mainClass="com.javahelps.jpa.Main"
--- src/com/javahelps/jpa/Main.java
+++ src/com/javahelps/jpa/Main.java
@@ -15,9 +15,9 @@ public class Main {
     public static void main(String[] args) {
 
         // Create two Students
-        create(1, "Alice", 22); // Alice will get an id 1
-        create(2, "Bob", 20); // Bob will get an id 2
-        create(3, "Charlie", 25); // Charlie will get an id 3
+        create(11, "Alice", 22); // Alice will get an id 1
+        create(21, "Bob", 20); // Bob will get an id 2
+        create(31, "Charlie", 25); // Charlie will get an id 3
 
         // Update the age of Bob using the id
         upate(2, "Bob", 25);
